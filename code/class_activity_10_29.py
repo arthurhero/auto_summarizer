eac34c3e6ca32edaab6e845789a5563bae545895
HongyuanZhang <zhanghon@grinnell.edu>
2019-10-30 12:12:08 -0500

    almost complete;added many functions and reorganized code

--- code/class_activity_10_29.py
+++ code/class_activity_10_29.py
@@ -1,213 +1,133 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
+'''
+Class Activity 3
+'''
 import os
 import glob
 import re
 import nltk
-from sklearn.metrics import jaccard_score
+
+# Note: first make sure that your current working directory is the code folder!
 path = '../summarization_dataset_duc_2004/test_docs'
 gspath = '../summarization_dataset_duc_2004/gold_summaries'
 
 
-# In[2]:
-
-
+# all subfolders
 def listdir_nohidden(p):
     return glob.glob(os.path.join(p, '*'))
 
 
-# In[3]:
-
-
-all_dirs = listdir_nohidden(path)
-print(all_dirs)
-
-
-# In[4]:
-
-
+# list of file names in d
 def get_file_names(d):
     all_files = os.listdir(d)
-    #lst = all_files.split('\n')
     if '.DS_Store' in all_files:
         all_files.remove('.DS_Store')
     return all_files
 
-some_files = get_file_names(all_dirs[0])
-print(some_files)
+
+def clean_doc(doc):
+    doc = doc[5:-2]
+    paragraph = ' '.join(doc)
+    return re.sub('\s+',' ', paragraph)
 
 
-# In[5]:
+def clean_summary(summary):
+    summary = summary[5:-2][0]
+    summary = re.sub('\<.+?\>', '', summary)
+    summary = summary[3:]
+    return re.sub('\s+',' ', summary)
 
 
-#get corresponding human-written summary files
-gs_dirs = listdir_nohidden(gspath)
-print(gs_dirs)
-
-cor_summary_dir = os.path.split(all_dirs[0])[-1]
-print(cor_summary_dir)
-some_standard_summary_output = get_file_names(gspath+'/'+cor_summary_dir)
-print(some_standard_summary_output)
-
-
-# In[6]:
-
-
-#read documents
-file_0 = some_files[0]
-f = open(all_dirs[0] + '/' + file_0, 'r')
-text = f.readlines()
-f.close()
-print(text)
-print(len(text))
-
-
-# In[7]:
-
-
-#cleaning
-text = text[:-2]
-print(text)
-text = text[5:]
-print(len(text))
-print(text)
-
-
-# In[8]:
-
-
-#read human summary filenames for file_0
-standard_summaries_file_0 = []
-for fname in some_standard_summary_output:
-    if file_0 in fname:
-        #print(fname)
-        standard_summaries_file_0.append(fname)
-print(standard_summaries_file_0)
-
-
-# In[9]:
-
-summaries=list()
-
-#read human summaries
-for f in standard_summaries_file_0:
-    #print(f)
-    if file_0 in f:
-        print(file_0)
-        print(f)
-        p = open(gspath+'/'+ cor_summary_dir +'/'+ f, 'r')
-        sl = p.readlines()
-        p.close()
-        sl = sl[5:-2]
-        only_summary = sl[0]
-        only_summary = re.sub('\<.+?\>', '', only_summary)
-        only_summary = only_summary[3:]
-        print(only_summary)
-        summaries.append(only_summary)
-
-
-
-# In[10]:
+# read all documents/summaries and clean
+def read_files(p):
+    dirs = listdir_nohidden(p)
+    file_paths = []
+    for dir in dirs:
+        file_names = get_file_names(dir)
+        file_paths.extend([dir + '/' + file_name for file_name in file_names])
+    files = []
+    for file_path in file_paths:
+        f = open(file_path, 'r')
+        text = f.readlines()
+        f.close()
+        #cleaning
+        if 'test_docs' in p:
+            text = clean_doc(text)
+        if 'gold_summaries' in p:
+            text = clean_summary(text)
+        files.append(text)
+    return files
 
 
 #write a function to calculate the KAPPA coeffieicents between each pair of human annotators for each document
 
 
-# In[11]:
-
-
 # implement PAGE RANK like algorithm to rank the sentences in each document and return the one with highest score as 
-#the summary of the document
+# the summary of the document
 # if that sounds scary, do the following:
 # find the words in a document 
 # define normalized-tf
 # for each sentence:
-#  calculate a score by adding the normalized tf-score of the words present in the document
+# calculate a score by adding the normalized tf-score of the words present in the document
 # for each sentence divide the total sentence score by the number of words (normalize as longer sentences might get more scores)
 # return the highest ranked sentence as an extractive summary
-
-def get_sentence(sentences):
+def get_sentence(text):
     '''
     get a list of sentences
     return the most important sentence
     '''
-    words=dict()
-    sents=list()
+    sentences = nltk.tokenize.sent_tokenize(text)
+    words = dict()
+    words_in_sents = list()
     for s in sentences:
-        sent=list()
+        words_in_sent = list()
         tokens_raw = nltk.word_tokenize(s)
         for t in tokens_raw:
-            if t.isalpha() and len(t)>1:
-                w=t.lower()
-                sent.append(w)
+            if t.isalpha() and len(t) > 1:
+                w = t.lower()
+                words_in_sent.append(w)
                 if w in words.keys():
-                    words[w]+=1.0
+                    words[w] += 1.0
                 else:
-                    words[w]=1.0
-        sents.append(sent)
-    s_score=list()
-    for s in sents:
-        score=0.0
+                    words[w] = 1.0
+        words_in_sents.append(words_in_sent)
+    num_words = sum(words.values())
+    for word in words:
+        words[word] /= num_words
+    s_score = list()
+    for s in words_in_sents:
+        score = 0.0
         for w in s:
-            score+=words[w]
-        score/=len(s)
+            score += words[w]
+        score /= len(s)
         s_score.append(score)
     print(s_score)
-    m=0.0
-    m_index=0
-    for i in range(len(s_score)):
-        s=s_score[i]
-        if s>m:
-            m=s
-            m_index=i
+    m_index = s_score.index(max(s_score))
     return sentences[m_index]
 
 
-print(text)
-sent=get_sentence(text)
-print(sent)
+#compare the similarity between your summary with each human-annotator's summary and return an average
+#use jaccards simialrity
+def jaccard(a, b):
+    a = set(a)
+    b = set(b)
+    c = a.intersection(b)
+    return float(len(c)) / (len(a) + len(b) - len(c))
 
-
-# In[12]:
 
 def clean(text):
-    words=list()
-    token=nltk.word_tokenize(text)
-    for t in token:
-        if t.isalpha() and len(t)>1:
-            w=t.lower()
+    words = []
+    tokens_raw = nltk.word_tokenize(text)
+    for t in tokens_raw:
+        if t.isalpha() and len(t) > 1:
+            w = t.lower()
             words.append(w)
     return words
 
-def intersection(lst1, lst2):
-    lst3 = [value for value in lst1 if value in lst2]
-    return lst3
-
-#compare the similarity between your summary with each human-annotator's summary and return an average
-#use jaccards simialrity
 
 # my_summary: a string, result returned by get_sentence
 # gold_summary: a string, corresponding gold summary sentence
-def similarity_score(my_summary, gold_summaries):
-    scores=list()
-    for gs in gold_summaries:
-        gs=clean(gs)
-        my=clean(my_summary)
-        inter=intersection(gs,my)
-        al=gs
-        al.extend(my)
-        gs=set(gs)
-        my=set(my)
-        al=set(al)
-        inter=set(inter)
-        scores.append(len(inter)/len(al))
-    ss=sum(scores)
-    ss/=len(scores)
-    return ss
+def similarity_score(my_summary, gold_summary):
+    return jaccard(clean(my_summary), clean(gold_summary))
 
 
 # my summaries: a list of strings, where each string is a summary sentence returned by get_sentence
@@ -215,12 +135,18 @@ def similarity_score(my_summary, gold_summaries):
 def evaluate_model(my_summaries, gold_summaries):
     accum_jaccard = 0
     num_of_doc = len(my_summaries)
+    num_of_summary = len(gold_summaries)
     for i in range(num_of_doc):
         accum_jaccard += similarity_score(my_summaries[i], gold_summaries[i])
-    return accum_jaccard/num_of_doc
+    for i in range(num_of_doc):
+        accum_jaccard += similarity_score(my_summaries[i], gold_summaries[num_of_doc+i])
+    for i in range(num_of_doc):
+        accum_jaccard += similarity_score(my_summaries[i], gold_summaries[2*num_of_doc+i])
+    for i in range(num_of_doc):
+        accum_jaccard += similarity_score(my_summaries[i], gold_summaries[3*num_of_doc+i])
+    return accum_jaccard/num_of_summary
 
-
-print(summaries)
-score=similarity_score(sent, summaries)
-print(score)
-
+docs = read_files(path)
+gold_summaries = read_files(gspath)
+my_summaries = [get_sentence(doc) for doc in docs]
+print(evaluate_model(my_summaries, gold_summaries))
